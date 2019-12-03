8829fdd43257810f1eccba2849ec197f164bda4c
My Name <my-name@chromium.org>
2019-12-03 20:30:35 +0800

    realtime message queue

--- DirectXTutorial/DirectXTutorial/main.cpp
+++ DirectXTutorial/DirectXTutorial/main.cpp
@@ -11,32 +11,11 @@
 #define WINDOW_TITLE L"³ÌÐòºËÐÄ¿ò¼Ü"
 #define WINDOW_CLASS_NAME L"My_CLASS"
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
-{
-	switch (uMessage)
-	{
-	case WM_PAINT:
-		ValidateRect(hWnd, NULL);
-		break;
-	case WM_KEYDOWN:
-		if (VK_ESCAPE == wParam)
-		{
-			DestroyWindow(hWnd);
-		}
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hWnd, uMessage, wParam, lParam);
-		break;
-	}
-
-	return 0;
-}
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int iCmdShow)
 {
+	HWND hWnd;
 	WNDCLASSEX wndClass = { 0 };
 	wndClass.cbSize = sizeof(WNDCLASSEX);
 	wndClass.style = CS_HREDRAW | CS_VREDRAW;
@@ -44,9 +23,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wndClass.cbWndExtra = 0;
 	wndClass.cbClsExtra = 0;
 	wndClass.hInstance = hInstance;
-	wndClass.hIcon = (HICON)::LoadImage(NULL, _T("icon.ico"), IMAGE_ICON, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
+	//wndClass.hIcon = (HICON)::LoadImage(NULL, _T("icon.ico"), IMAGE_ICON, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
 	wndClass.hCursor = ::LoadCursor(NULL, IDC_ARROW);
-	wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
 	wndClass.lpszMenuName = NULL;
 	wndClass.lpszClassName = WINDOW_CLASS_NAME;
 
@@ -55,23 +34,41 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return -1;
 	}
 
-	HWND hWnd = CreateWindow(WINDOW_CLASS_NAME, WINDOW_TITLE, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
-	MoveWindow(hWnd, 250, 250, WINDOW_WIDTH, WINDOW_HEIGHT, TRUE);
+	hWnd = CreateWindowEx(0, WINDOW_CLASS_NAME, WINDOW_TITLE, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
+	//MoveWindow(hWnd, 250, 250, WINDOW_WIDTH, WINDOW_HEIGHT, TRUE);
 
 	ShowWindow(hWnd, iCmdShow);
-	UpdateWindow(hWnd);
+	//UpdateWindow(hWnd);
 
 	MSG msg = { 0 };
-	while (msg.message != WM_QUIT)
+	while (TRUE)
 	{
-		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
+			if (msg.message == WM_QUIT)
+				goto exit;
+
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
+
+		// run render code
 	}
 
-	UnregisterClass(WINDOW_CLASS_NAME, wndClass.hInstance);
+exit:
+	return msg.wParam;
+}
 
-	return 0;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMessage)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		break;
+	}
+
+	return DefWindowProc(hWnd, uMessage, wParam, lParam);
 }

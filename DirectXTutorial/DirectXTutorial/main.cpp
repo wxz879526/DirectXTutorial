0f5c08ecc7cdb26c06e028315cda3343e085f36c
My Name <my-name@chromium.org>
2019-12-09 17:05:33 +0800

    update

--- DirectXTutorial/DirectXTutorial/main.cpp
+++ DirectXTutorial/DirectXTutorial/main.cpp
@@ -1,202 +1,274 @@
-//#include "stdafx.h"
+// include the basic windows header files and the Direct3D header file
 #include <windows.h>
-#include <atldbcli.h>
-#include <playsoundapi.h>
+#include <windowsx.h>
 #include <d3d9.h>
-#include <D3dx9math.h>
+#include <d3dx9.h>
 
-#pragma comment(lib, "d3d9.lib")
-#pragma comment(lib, "d3dx9d.lib")
+// define the screen resolution
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+// include the Direct3D Library files
+#pragma comment (lib, "d3d9.lib")
+#pragma comment (lib, "d3dx9.lib")
+
+// global declarations
+LPDIRECT3D9 d3d;    // the pointer to our Direct3D interface
+LPDIRECT3DDEVICE9 d3ddev;    // the pointer to the device class
+LPDIRECT3DVERTEXBUFFER9 v_buffer = NULL;    // the pointer to the vertex buffer
+LPDIRECT3DINDEXBUFFER9 i_buffer = NULL;  // create index buffer
 
 
-#pragma comment(lib, "winmm.lib")
 
-#define WINDOW_WIDTH 1920
-#define WINDOW_HEIGHT 1080
+// function prototypes
+void initD3D(HWND hWnd);    // sets up and initializes Direct3D
+void render_frame(void);    // renders a single frame
+void cleanD3D(void);    // closes Direct3D and releases memory
+void init_graphics(void);    // 3D declarations
 
-#define WINDOW_TITLE L"程序核心框架"
-#define WINDOW_CLASS_NAME L"My_CLASS"
-
-LPDIRECT3D9 d3d;
-LPDIRECT3DDEVICE9 d3ddevice;
-LPDIRECT3DVERTEXBUFFER9 v_buffer;
-
-void initD3D(HWND hWnd); // sets up and initializes Direct3D
-void initGraphics();
-void render_frame();    // renders a single frame
-void cleanD3D();       // closes Direct3D and releases memory
-
-struct CUSTOMVERTEX
-{
-	float x, y, z;
-	DWORD color;
-};
-
+struct CUSTOMVERTEX { FLOAT X, Y, Z; DWORD COLOR; };
 #define CUSTOMFVF (D3DFVF_XYZ | D3DFVF_DIFFUSE)
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam);
+// the WindowProc function prototype
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int iCmdShow)
+
+// the entry point for any Windows program
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
 {
 	HWND hWnd;
-	WNDCLASSEX wndClass = { 0 };
-	wndClass.cbSize = sizeof(WNDCLASSEX);
-	wndClass.style = CS_HREDRAW | CS_VREDRAW;
-	wndClass.lpfnWndProc = WndProc;
-	wndClass.cbWndExtra = 0;
-	wndClass.cbClsExtra = 0;
-	wndClass.hInstance = hInstance;
-	//wndClass.hIcon = (HICON)::LoadImage(NULL, _T("icon.ico"), IMAGE_ICON, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
-	wndClass.hCursor = ::LoadCursor(NULL, IDC_ARROW);
-	//wndClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
-	wndClass.lpszMenuName = NULL;
-	wndClass.lpszClassName = WINDOW_CLASS_NAME;
+	WNDCLASSEX wc;
 
-	if (!RegisterClassEx(&wndClass))
-	{
-		return -1;
-	}
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
 
-	hWnd = CreateWindowEx(0, 
-		WINDOW_CLASS_NAME, 
-		WINDOW_TITLE, 
-		WS_POPUP, 
-		0, 0, 
-		WINDOW_WIDTH, WINDOW_HEIGHT, 
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.lpszClassName = L"WindowClass";
+
+	RegisterClassEx(&wc);
+
+	hWnd = CreateWindowEx(NULL, L"WindowClass", L"Our Direct3D Program",
+		WS_OVERLAPPEDWINDOW, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT,
 		NULL, NULL, hInstance, NULL);
-	//MoveWindow(hWnd, 250, 250, WINDOW_WIDTH, WINDOW_HEIGHT, TRUE);
 
-	ShowWindow(hWnd, iCmdShow);
-	//UpdateWindow(hWnd);
+	ShowWindow(hWnd, nCmdShow);
 
+	// set up and initialize Direct3D
 	initD3D(hWnd);
 
-	MSG msg = { 0 };
+	// enter the main loop:
+
+	MSG msg;
+
 	while (TRUE)
 	{
 		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
-			if (msg.message == WM_QUIT)
-				goto exit;
-
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
 
-		// run render code
+		if (msg.message == WM_QUIT)
+			break;
+
 		render_frame();
 	}
 
-exit:
+	// clean up DirectX and COM
 	cleanD3D();
+
 	return msg.wParam;
 }
 
+
+// this is the main message handler for the program
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+		return 0;
+	} break;
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+
+// this function initializes and prepares Direct3D for use
 void initD3D(HWND hWnd)
 {
 	d3d = Direct3DCreate9(D3D_SDK_VERSION);
+
 	D3DPRESENT_PARAMETERS d3dpp;
+
 	ZeroMemory(&d3dpp, sizeof(d3dpp));
-	d3dpp.hDeviceWindow = hWnd;
-	d3dpp.Windowed = FALSE;
+	d3dpp.Windowed = TRUE;
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.hDeviceWindow = hWnd;
 	d3dpp.BackBufferFormat = D3DFMT_X8R8G8B8;
-	d3dpp.BackBufferWidth = WINDOW_WIDTH;
-	d3dpp.BackBufferHeight = WINDOW_HEIGHT;
-	d3dpp.EnableAutoDepthStencil = TRUE;
-	d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+	d3dpp.BackBufferWidth = SCREEN_WIDTH;
+	d3dpp.BackBufferHeight = SCREEN_HEIGHT;
+	d3dpp.EnableAutoDepthStencil = TRUE;    // automatically run the z-buffer for us
+	d3dpp.AutoDepthStencilFormat = D3DFMT_D16;    // 16-bit pixel format for the z-buffer
 
-	d3d->CreateDevice(D3DADAPTER_DEFAULT,  //使用默认图形卡
-		D3DDEVTYPE_HAL, //使用硬件处理渲染
-		hWnd, 
-		D3DCREATE_SOFTWARE_VERTEXPROCESSING, 
+	// create a device class using this information and the info from the d3dpp stuct
+	d3d->CreateDevice(D3DADAPTER_DEFAULT,
+		D3DDEVTYPE_HAL,
+		hWnd,
+		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
 		&d3dpp,
-		&d3ddevice);
-	d3ddevice->SetRenderState(D3DRS_LIGHTING, FALSE);
-	d3ddevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
-	d3ddevice->SetRenderState(D3DRS_ZENABLE, TRUE);
+		&d3ddev);
 
-	initGraphics();
+	init_graphics();    // call the function to initialize the triangle
+
+	d3ddev->SetRenderState(D3DRS_LIGHTING, FALSE);    // turn off the 3D lighting
+	d3ddev->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);    // both sides of the triangles
+	d3ddev->SetRenderState(D3DRS_ZENABLE, TRUE);    // turn on the z-buffer
 }
 
-void initGraphics()
+
+// this is the function used to render a single frame
+void render_frame(void)
 {
-	CUSTOMVERTEX vertices[] = {
-		{3.0f, -3.0f, 0.0f, D3DCOLOR_XRGB(0,0,255)},
-		{0.0f, 3.0f, 0.0f, D3DCOLOR_XRGB(0,255,0)},
-		{-3.0f, -3.0f, 0.0f, D3DCOLOR_XRGB(255,0,0)}
+	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+	d3ddev->Clear(0, NULL, D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+
+	d3ddev->BeginScene();
+
+	// select which vertex format we are using
+	d3ddev->SetFVF(CUSTOMFVF);
+
+	// set the view transform
+	D3DXMATRIX matView;    // the view transform matrix
+	D3DXMatrixLookAtLH(&matView,
+		&D3DXVECTOR3(0.0f, 0.0f, 25.0f),   // the camera position
+		&D3DXVECTOR3(0.0f, 0.0f, 0.0f),    // the look-at position
+		&D3DXVECTOR3(0.0f, 1.0f, 0.0f));    // the up direction
+	d3ddev->SetTransform(D3DTS_VIEW, &matView);    // set the view transform to matView
+
+	// set the projection transform
+	D3DXMATRIX matProjection;    // the projection transform matrix
+	D3DXMatrixPerspectiveFovLH(&matProjection,
+		D3DXToRadian(45),    // the horizontal field of view
+		(FLOAT)SCREEN_WIDTH / (FLOAT)SCREEN_HEIGHT, // aspect ratio
+		1.0f,		// the near view-plane
+		100.0f);   // the far view-plane
+	d3ddev->SetTransform(D3DTS_PROJECTION, &matProjection);     //set the projection
+
+	// select the vertex buffer to display
+	d3ddev->SetStreamSource(0, v_buffer, 0, sizeof(CUSTOMVERTEX));
+	d3ddev->SetIndices(i_buffer);
+
+	// draw the cube
+	d3ddev->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, 8, 0, 12);
+
+
+	D3DXMATRIX matTranslateA;    // a matrix to store the translation for triangle A
+	D3DXMATRIX matTranslateB;    // a matrix to store the translation for triangle B
+	D3DXMATRIX matRotateY;    // a matrix to store the rotation for each triangle
+	static float index = 0.0f; index += 0.03f; // an ever-increasing float value
+
+	static float z_index = 0.0f;
+	z_index += 0.03f;
+	// build MULTIPLE matrices to translate the model and one to rotate
+	D3DXMatrixTranslation(&matTranslateA, z_index, 0.0f, 0.0f);
+	D3DXMatrixTranslation(&matTranslateB, 0.0f, 0.0f, -2.0f);
+	D3DXMatrixRotationY(&matRotateY, index);    // the front side
+
+	// tell Direct3D about each world transform, and then draw another triangle
+	d3ddev->SetTransform(D3DTS_WORLD, &(matTranslateA));
+	//d3ddev->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2);
+
+	//d3ddev->SetTransform(D3DTS_WORLD, &(matTranslateB * matRotateY));
+	//d3ddev->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2);
+
+	d3ddev->EndScene();
+	d3ddev->Present(NULL, NULL, NULL, NULL);
+}
+
+
+// this is the function that cleans up Direct3D and COM
+void cleanD3D(void)
+{
+	v_buffer->Release();    // close and release the vertex buffer
+	d3ddev->Release();    // close and release the 3D device
+	d3d->Release();    // close and release Direct3D
+}
+
+
+// this is the function that puts the 3D models into video RAM
+// this is the function that puts the 3D models into video RAM
+void init_graphics(void)
+{
+	// create the vertices using the CUSTOMVERTEX struct
+	CUSTOMVERTEX vertices[] =
+	{
+		{ -3.0f, 3.0f, -3.0f, D3DCOLOR_XRGB(0, 0, 255), },
+		{ 3.0f, 3.0f, -3.0f, D3DCOLOR_XRGB(0, 255, 0), },
+		{ -3.0f, -3.0f, -3.0f, D3DCOLOR_XRGB(255, 0, 0), },
+		{ 3.0f, -3.0f, -3.0f, D3DCOLOR_XRGB(0, 255, 255), },
+		{ -3.0f, 3.0f, 3.0f, D3DCOLOR_XRGB(0, 0, 255), },
+		{ 3.0f, 3.0f, 3.0f, D3DCOLOR_XRGB(255, 0, 0), },
+		{ -3.0f, -3.0f, 3.0f, D3DCOLOR_XRGB(0, 255, 0), },
+		{ 3.0f, -3.0f, 3.0f, D3DCOLOR_XRGB(0, 255, 255), },
 	};
 
-	d3ddevice->CreateVertexBuffer(3 * sizeof(CUSTOMVERTEX),
-		0, CUSTOMFVF, D3DPOOL_MANAGED, &v_buffer, nullptr);
-	VOID *pData;
-	v_buffer->Lock(0, 0, &pData, 0);
-	memcpy(pData, vertices, sizeof(vertices));
+	// create a vertex buffer interface called v_buffer
+	d3ddev->CreateVertexBuffer(8 * sizeof(CUSTOMVERTEX),
+		0,
+		CUSTOMFVF,
+		D3DPOOL_MANAGED,
+		&v_buffer,
+		NULL);
+
+	VOID* pVoid;    // a void pointer
+
+	// lock v_buffer and load the vertices into it
+	v_buffer->Lock(0, 0, (void**)&pVoid, 0);
+	memcpy(pVoid, vertices, sizeof(vertices));
 	v_buffer->Unlock();
-}
 
-void render_frame()
-{
-	//类似画背景
-	d3ddevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
-	d3ddevice->Clear(0, NULL, D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 0), 0.0f, 0);
-
-	//类似画前景内容
-	d3ddevice->BeginScene(); //锁住显卡的内存
-
-	d3ddevice->SetFVF(CUSTOMFVF);
-
-	D3DXMATRIX matView;
-	D3DXMatrixLookAtLH(&matView, 
-		&D3DXVECTOR3(0.0f, 0.0f, 15.0f),
-		&D3DXVECTOR3(0.0f, 0.0f, 0.0f),
-		&D3DXVECTOR3(0.0f, 1.0f, 0.0f));
-	d3ddevice->SetTransform(D3DTS_VIEW, &matView);
-
-	D3DXMATRIX matProject;
-	D3DXMatrixPerspectiveFovLH(&matProject,
-		D3DXToRadian(45),
-		(FLOAT)WINDOW_WIDTH / (FLOAT)WINDOW_HEIGHT,
-		1.0f, 100.0f);
-	d3ddevice->SetTransform(D3DTS_PROJECTION, &matProject);
-
-	d3ddevice->SetStreamSource(0, v_buffer, 0, sizeof(CUSTOMVERTEX));
-
-	D3DXMATRIX matTranslateA;
-	D3DXMATRIX matTranslateB;
-	D3DXMATRIX matRotateY;
-	static float index = 0.0f;
-	index += 0.05f;
-	D3DXMatrixTranslation(&matTranslateA, 0.0f, 0.0f, 2.0f);
-	D3DXMatrixTranslation(&matTranslateB, 0.0f, 0.0f, -2.0f);
-	D3DXMatrixRotationY(&matRotateY, index);
-	d3ddevice->SetTransform(D3DTS_WORLD, &(matTranslateA*matRotateY));
-	d3ddevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
-
-	d3ddevice->SetTransform(D3DTS_WORLD, &(matTranslateB*matRotateY));
-	d3ddevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
-
-	d3ddevice->EndScene();
-	d3ddevice->Present(NULL, NULL, NULL, NULL);
-}
-
-void cleanD3D()
-{
-	v_buffer->Release();
-	d3ddevice->Release();
-	d3d->Release();
-}
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
-{
-	switch (uMessage)
+	// create the indices using an int array
+	short indices[] =
 	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	default:
-		break;
-	}
+		0, 1, 2,    // side 1
+		2, 1, 3,
 
-	return DefWindowProc(hWnd, uMessage, wParam, lParam);
+		2, 0, 6,    // side 2
+		6, 0, 4,
+
+		4, 5, 6,    // side 3
+		6, 5, 7,
+		
+		1, 5, 3,    // side 4
+		3, 5, 7,
+
+		4, 0, 5,    // side 5
+		5, 0, 1,
+
+		6, 2, 7,    // side 6
+		7, 2, 3,
+	};
+
+	// create an index buffer interface called i_buffer
+	d3ddev->CreateIndexBuffer(36 * sizeof(short),
+		0,
+		D3DFMT_INDEX16,
+		D3DPOOL_MANAGED,
+		&i_buffer,
+		NULL);
+
+	// lock i_buffer and load the indices into it
+	i_buffer->Lock(0, 0, (void**)&pVoid, 0);
+	memcpy(pVoid, indices, sizeof(indices));
+	i_buffer->Unlock();
 }
